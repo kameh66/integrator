a99eca282c4583fff2d184a98ec2d60594470c68
Ilya Rogov <ilyar.software@gmail.com>
2018-03-29 19:03:05 +0300

    Optimize DeskTime API

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const config = require('./lib/config').integrator
 const jira = require('./lib/jira')
 const desktime = require('./lib/desktime')
 
 desktime.fetchMyTime(function(time) {
+  const workLog = config.desktimeToJira.workLog
+
   time.forEach(function(item) {
-    jira.addWorkLog(item.issueKey, item.time, 'Logged via DeskTime')
+    jira.addWorkLog(item[workLog.use], item.time, workLog.comment)
   })
 })
